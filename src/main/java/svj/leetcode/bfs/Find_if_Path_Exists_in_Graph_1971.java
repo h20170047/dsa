6e14efd8d22109068ceab2168bbdf909868d4d22
h20170047 <46812751+h20170047@users.noreply.github.com>
2023-01-28 01:05:42 +0530

    1971 Find_if_Path_Exists_in_Graph

---
+++ src/main/java/svj/leetcode/bfs/Find_if_Path_Exists_in_Graph_1971.java
@@ -0,0 +1,37 @@
+package svj.leetcode.bfs;
+
+import java.util.*;
+
+public class Find_if_Path_Exists_in_Graph_1971 {
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        if(source== destination)
+            return true;
+        // get list of adj nodes
+        Map<Integer, List<Integer>> neighbors= new HashMap<>();
+        List<Integer> adj;
+        int src, dsn, procNode = 0;
+        for(int[] edge: edges){
+            src= edge[0]; dsn= edge[1];
+            // bi-directional edges
+            adj = neighbors.getOrDefault(src, new LinkedList<>()); adj.add(dsn); neighbors.put(src, adj);
+            adj= neighbors.getOrDefault(dsn, new LinkedList<>()); adj.add(src); neighbors.put(dsn, adj);
+        }
+        Deque<Integer> nearNodes= new LinkedList<>();
+        Map<Integer, Boolean> visited= new HashMap();
+        nearNodes.add(source);
+        visited.put(source, true);
+        while(! nearNodes.isEmpty()){
+            procNode= nearNodes.remove();
+            adj= neighbors.getOrDefault(procNode, new LinkedList<>());
+            for(Integer node: adj){
+                if(node== destination)
+                    return true;
+                if(visited.getOrDefault(node, false)== false){
+                    visited.put(node, true);
+                    nearNodes.add(node);
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ src/test/java/svj/leetcode/bfs/Find_if_Path_Exists_in_Graph_1971Test.java
@@ -0,0 +1,35 @@
+package svj.leetcode.bfs;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class Find_if_Path_Exists_in_Graph_1971Test {
+
+    private Find_if_Path_Exists_in_Graph_1971 instance;
+
+    @BeforeEach
+    public void setup(){
+        instance= new Find_if_Path_Exists_in_Graph_1971();
+    }
+
+    @Test
+    void test1() {
+        boolean result = instance.validPath(3, new int[][]{{0, 1}, {1, 2}, {2, 0}}, 0, 2);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void test2() {
+        boolean result = instance.validPath(6, new int[][]{{0,1},{0,2},{3,5},{5,4},{4,3}}, 0, 5);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    void test3() {
+        boolean result = instance.validPath(1, new int[][]{{}}, 0, 0);
+        assertThat(result).isTrue();
+    }
+}
