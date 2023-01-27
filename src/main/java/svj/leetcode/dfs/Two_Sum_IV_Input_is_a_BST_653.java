55f4c8b83d7bb5031ade2ac30320a90d71fd5165
h20170047 <46812751+h20170047@users.noreply.github.com>
2023-01-28 00:06:18 +0530

    653 Two_Sum_IV_Input_is_a_BST

---
+++ src/main/java/svj/leetcode/dfs/Two_Sum_IV_Input_is_a_BST_653.java
@@ -0,0 +1,60 @@
+package svj.leetcode.dfs;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class Two_Sum_IV_Input_is_a_BST_653 {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public TreeNode createTreeNode(Integer[] arr){
+        return helperTreeCreation(arr, 0);
+    }
+
+    private TreeNode helperTreeCreation(Integer[] arr, int i) {
+        if(i>= arr.length || arr[i]== null)
+            return null;
+        return new TreeNode(arr[i], helperTreeCreation(arr, 2*i+1), helperTreeCreation(arr, 2*i+2));
+    }
+
+    public boolean findTarget(TreeNode root, int k) {
+        Deque<TreeNode> stack= new LinkedList<>();
+        stack.push(root);
+        while(! stack.isEmpty()){
+            TreeNode processing= stack.pop(); // always push non-null values to stack
+            // search if we have k-p.val in BST
+            int toFind= k- processing.val;
+            if(find(root, processing, toFind))
+                return true;
+            if(processing.left!= null)
+                stack.push(processing.left);
+            if(processing.right!= null)
+                stack.push(processing.right);
+        }
+        return false;
+    }
+
+    private boolean find(TreeNode root, TreeNode processing, int toFind) {
+        if(root!= null && toFind< root.val)
+            return find(root.left, processing, toFind);
+        else if(root!= null && toFind> root.val)
+            return find(root.right, processing, toFind);
+        else if(root!= null && toFind== root.val){
+            if(root!= processing)
+                return true;
+            else return find(root.left, processing, toFind);
+        }
+        return false;
+    }
+}
---
+++ src/test/java/svj/leetcode/dfs/Two_Sum_IV_Input_is_a_BST_653Test.java
@@ -0,0 +1,31 @@
+package svj.leetcode.dfs;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class Two_Sum_IV_Input_is_a_BST_653Test {
+
+    private Two_Sum_IV_Input_is_a_BST_653 instance;
+
+    @BeforeEach
+    public void setup(){
+        instance= new Two_Sum_IV_Input_is_a_BST_653();
+    }
+
+    @Test
+    public void test1(){
+        Two_Sum_IV_Input_is_a_BST_653.TreeNode rootNode = instance.createTreeNode(new Integer[]{5, 3, 6, 2, 4, null, 7});
+        boolean result = instance.findTarget(rootNode, 9);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void test2(){
+        Two_Sum_IV_Input_is_a_BST_653.TreeNode rootNode = instance.createTreeNode(new Integer[]{5,3,6,2,4,null,7});
+        boolean result = instance.findTarget(rootNode, 28);
+        assertThat(result).isFalse();
+    }
+
+}
